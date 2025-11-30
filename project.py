faed145b75ace487fa1626c0e1cf75cf339c4868
dymp1q <dmytrivmaryan2008@gmail.com>
2025-11-30 14:38:17 +0200

    Implement graph reading function
    Added a function to read a graph from a file and return its adjacency list.

--- project.py
+++ project.py
@@ -2,6 +2,42 @@
 
 import argparse
 
+def read_graph(filepath, directed = False):
+    """This function reads graps
+    """
+
+    adjacency_dict = {}
+
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            parts = line.strip().split()
+            if len(parts) == 2:
+                try:
+                    u = int(parts[0])
+                    v = int(parts[1])
+                except ValueError:
+                    u = parts[0]
+                    v = parts[1]
+
+                if u not in adjacency_dict:
+                    adjacency_dict[u] = set()
+                adjacency_dict[u].add(v)
+
+                if not directed:
+                    if v not in adjacency_dict:
+                        adjacency_dict[v] = set()
+                    adjacency_dict[v].add(u)
+
+
+                if v not in adjacency_dict:
+                    adjacency_dict[v] = set()
+
+        final_graph = {node: list(neighbours) for node, neighbours in adjacency_dict.items()}
+
+        return final_graph
+
+#print(read_graph("test_undirected.txt"))
+
 def input_graph_visualisation(graph: dict):
     '''
     Друкує вхідний граф у форматі "u -> v"
